b7efeb0c38d72a9ac825594b1abc6f7fcfcad03c
Alphan Yang <ay329@scarletmail.rutgers.edu>
2021-12-10 09:22:46 -0500

    search

--- app/src/main/java/com/example/android40/SearchActivity.java
+++ app/src/main/java/com/example/android40/SearchActivity.java
@@ -71,6 +71,13 @@ public class SearchActivity extends AppCompatActivity {
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         conjuncSpinner.setAdapter(adapter3);
 
+        searchedImage = findViewById(R.id.recyclerViewSearch);
+        searchedImage.setLayoutManager(new LinearLayoutManager(this));
+        sa = new SearchAdapter(this, searchedPhotoList);
+        searchedImage.setAdapter(sa);
+        searchedImage.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        sa.notifyDataSetChanged();
+
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,45 +103,97 @@ public class SearchActivity extends AppCompatActivity {
                 if (conjuncSpinner.getSelectedItem().toString().equals("AND")) {
                     matchNeeded = 2;
                 }
-                for (Photo p : allPhotoList) {
+
+                for(int i = 0; i < allPhotoList.size(); i ++){
                     int numMatch = 0;
-                    for (int i = 0; i < typeArr.length; i++) {
-                        // If its blank consider it to be a match always
+                    for (int j = 0; j < typeArr.length; j++){
                         if (typeArr[i].isEmpty()) {
                             numMatch++;
                             continue;
                         }
-                        // If not blank, check the photos
-                        boolean containsDescription = p.getTagHashMap().containsKey(descArr[i]);
-                        if (!containsDescription) {
-                            continue;
-                        }
-                        boolean containsType = p.getTagHashMap().get(descArr[i]).getType().equals(typeArr[i]);
-                        if (containsDescription && containsType) {
-                            numMatch++;
+                        for(int k = 0; k < allPhotoList.get(i).getSortedTags().size(); k++){
+                            int res = isSubstring(descArr[j], allPhotoList.get(i).getSortedTags().get(k).getDesc());
+                            if(res == -1){
+                                continue;
+                            }
+                            boolean containsType = allPhotoList.get(i).getSortedTags().get(k).getType().equals(typeArr[i]);
+                            if(res != -1 && containsType){
+                                numMatch++;
+                            }
                         }
                     }
                     if (numMatch >= matchNeeded) {
-                        searchedPhotoList.add(p);
+                        searchedPhotoList.add(allPhotoList.get(i));
 
                         //addPhotoToList(p);
                     }
                 }
+//                for (Photo p : allPhotoList) {
+//                    int numMatch = 0;
+//                    for (int i = 0; i < typeArr.length; i++) {
+//                        // If its blank consider it to be a match always
+//                        if (typeArr[i].isEmpty()) {
+//                            numMatch++;
+//                            continue;
+//                        }
+////                        int res = isSubstring(descArr[i], p.getTagHashMap().)
+//                        // If not blank, check the photos
+//                        boolean containsDescription = p.getTagHashMap().containsKey(descArr[i]);
+//                        if (!containsDescription) {
+//                            continue;
+//                        }
+//                        boolean containsType = p.getTagHashMap().get(descArr[i]).getDesc().equals(typeArr[i]);
+//                        if (containsDescription && containsType) {
+//                            numMatch++;
+//                        }
+//                    }
+//                    if (numMatch >= matchNeeded) {
+//                        searchedPhotoList.add(p);
+//
+//                        //addPhotoToList(p);
+//                    }
+//                }
+
+
                 if(searchedPhotoList.size() == 0){
                     Toast.makeText(SearchActivity.this, "No matches found", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(SearchActivity.this, "Found matches", Toast.LENGTH_LONG).show();
                 }
+                searchedImage = findViewById(R.id.recyclerViewSearch);
+                searchedImage.setLayoutManager(new LinearLayoutManager(SearchActivity.this));
+                sa = new SearchAdapter(SearchActivity.this, searchedPhotoList);
+                searchedImage.setAdapter(sa);
+                searchedImage.addItemDecoration(new DividerItemDecoration(SearchActivity.this, LinearLayoutManager.VERTICAL));
                 sa.notifyDataSetChanged();
 
             }
         });
 
-        searchedImage = findViewById(R.id.recyclerViewSearch);
-        searchedImage.setLayoutManager(new LinearLayoutManager(this));
-        sa = new SearchAdapter(this, searchedPhotoList);
-        searchedImage.setAdapter(sa);
-        searchedImage.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
-        sa.notifyDataSetChanged();
+
+    }
+
+    static int isSubstring(
+            String s1, String s2)
+    {
+        int M = s1.length();
+        int N = s2.length();
+
+        /* A loop to slide pat[] one by one */
+        for (int i = 0; i <= N - M; i++) {
+            int j;
+
+            /* For current index i, check for
+            pattern match */
+            for (j = 0; j < M; j++)
+                if (s2.charAt(i + j)
+                        != s1.charAt(j))
+                    break;
+
+            if (j == M)
+                return i;
+        }
+
+        return -1;
     }
 }
